ba0fcb55de4620f191d11b4203edb04daca101eb
Odd Erik Haraldsen <Erik.har@hotmail.no>
2019-12-16 19:36:18 +0100

    started on about page

--- src/About/about.js
+++ src/About/about.js
@@ -1,11 +1,29 @@
 import React from 'react';
+import './about.css'
 
 const About = () => {
     return (
-        <div>
-
-        About
-
+        <div className="about-container">
+            <div className="about-wrapper">
+            <div>
+            image
+            </div>
+            <div>
+                <h1>Odd Erik Haraldsen</h1>
+                <h3>Who am I</h3>
+                <p>
+                lorem ipsum
+                </p>
+                <h3>Toolbox</h3>
+                <p>
+                    lorem ipsum 2
+                </p>
+                <h3>Education</h3>
+                <p>
+                    lorem ipsum 3
+                </p>
+            </div>
+            </div>
         </div>
     );
 }
--- src/Home/Projects/Github-home/Github_home.js
+++ src/Home/Projects/Github-home/Github_home.js
@@ -1,14 +1,13 @@
 import React, { Component } from 'react';
 import './github_home.css'
-import {Link} from 'react-router-dom'
 
 class Github_home extends Component {
     state = {  }
     render() {
         return (
-            <div className="project-background github-home">
+            <div className="project-background github-home ">
                 <a href="https://github.com/TheOddCaptain" target="_blank" className="link-nostyle">
-                <div className="project "><p className="block-focus">Github</p><p className="block-hover">View my GitHub</p></div>
+                <div className="project"><p className="block-focus">Github</p><p className="block-hover">View my GitHub</p></div>
                 </a>
             </div>
         );
