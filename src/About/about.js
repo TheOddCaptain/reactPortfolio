4ffd7e2bf2ca2cd3e44792288e1c8f73ab8276a2
Odd Erik Haraldsen <Erik.har@hotmail.no>
2019-12-18 23:58:57 +0100

    about and contact almost done

--- src/About/about.js
+++ src/About/about.js
@@ -5,24 +5,27 @@ const About = () => {
     return (
         <div className="about-container">
             <div className="about-wrapper">
-            <div>
-            image
+            <div className="about-image">
             </div>
-            <div>
-                <h1>Odd Erik Haraldsen</h1>
+                <div className="who-am-i about-background">
                 <h3>Who am I</h3>
                 <p>
-                lorem ipsum
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
                 </p>
+                </div>
+                <div className="toolbox about-background">
                 <h3>Toolbox</h3>
                 <p>
-                    lorem ipsum 2
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
                 </p>
+                </div>
+                <div className="education about-background">
                 <h3>Education</h3>
                 <p>
-                    lorem ipsum 3
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
                 </p>
-            </div>
+                </div>
+            
             </div>
         </div>
     );
--- src/Contact/contact.js
+++ src/Contact/contact.js
@@ -1,12 +1,42 @@
 import React from 'react';
+import './contact.css'
 
 const Contact = () => {
     return (
-        <div>
-            
-        Contact 
+<div className="form-container">
+  <div className="contact-form box">
+      <form action="https://formspree.io/erik.har@hotmail.no" method="POST" data-netlify="true">
+        <label>
+        Fullname/Organisation
+          <div className="text-box">
+            <input type="text" name="name" className="name" placeholder="Mandatory" autofocus/>
+          </div>
+        </label><br/>
+        <label>
+        E-mail
+          <div className="text-box">
+            <input type="email" name="email" className="email" placeholder="Mandatory"/>
+          </div>
+        </label><br/>
+        <label>
+        Phone
+          <div className="text-box">
+            <input type="tel" name="phone" pattern="[0-9]{8}" className="telephone" placeholder="Optional"/>
+          </div>
+        </label><br/>
+        <label>
+        Message
+          <div className="text-box">
+            <textarea name="message" className="message" rows="10" cols="100">Your message here:</textarea>
+          </div>
+        </label><br/>
+          <div  className="status"><br/>
+          </div>
+        <button type="submit" value="Send" className="btn">Send</button>
+    </form>
+  </div>
+</div>
 
-        </div>
     );
 }
 
